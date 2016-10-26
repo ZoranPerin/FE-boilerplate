412c384939ab09350817754b38c0bc70626303f5
Aleksandar Radivojevic <anagramik@gmail.com>
2016-10-26 14:36:23 +0200

    ADD: and new gulpfiles options
    This commit will add BrowserSync and new Gulp tasks.
    It will also add Normalize.css and remove reset scss file

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,29 @@ var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-
+var concat = require('gulp-concat');
+var combineMq = require('gulp-combine-mq');
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
-var uglify = require('gulp-uglifyjs');
+var uglify = require('gulp-uglify');
+
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+var scripts = [
+    'assets/js/vendors/jquery.min.js',
+    'assets/js/vendors/plugins.js',
+    'assets/js/main.js'
+];
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: '',
+       index: "index.html"
+    },
+  })
+})
 
 gulp.task('scss', function () {
     var processors = [
@@ -16,23 +35,36 @@ gulp.task('scss', function () {
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss(processors))
+        .pipe(combineMq({
+            beautify: false
+        }))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(gulp.dest('./assets/css'))
+        .pipe(browserSync.reload({
+          stream: true
+        }))
 });
 
 gulp.task('scripts', function () {
-    return gulp.src('./assets/js/**/*.js')
-        .pipe(uglify('scripts.min.js', {
-            outSourceMap: true
-        }))
-        .pipe(gulp.dest('./dist/js'));
+    return gulp.src( scripts )
+        .pipe(sourcemaps.init())
+        .pipe(concat('all.js'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./assets/js'));
 });
 
-gulp.task('watch', function () {
+//  templates
+gulp.task('templates', function() {
+    return gulp.src('**/*.html')
+        .on("end", reload);
+});
+
+gulp.task('watch', ['browserSync'], function () {
     gulp.watch('./assets/sass/**/*.scss', ['scss']);
     gulp.watch('./assets/js/**/*.js', ['scripts']);
+    gulp.watch('./**/*.html', ['templates']);
 });
 
-gulp.task('minifyjs', ['scripts']);
+gulp.task('minifyassets', ['scripts']);
 
 //gulp.task('default', ['scss']);
